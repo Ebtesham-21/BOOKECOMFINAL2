966dbc65148279dfa395a76e8ec10df7c4f5e0f8
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-09 17:15:41 +0600

    admin routing products

--- app/clientComponent.js
+++ app/clientComponent.js
@@ -1,35 +1,12 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
-import Nav from "./components/Nav";
+import Layout from "./components/Layout";
+
 
 export default function ClientComponent() {
-  const { data: session } = useSession();
-  if (!session) {
-    return (
-      <div className="bg-blue-900 w-screen h-screen flex items-center">
-        <div className="text-center w-full">
-          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">
-            Login With Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <>
-    <div className="bg-red-600 min-h-screen text-white flex">
-      <Nav/>
-      <div className="bg-white flex-grow text-black mt-2 mr-2 mb-0 p-10 rounded-lg p-4">
-
-      <p>Signed in as {session.user.email}</p>
-      <button onClick={() => signOut()}>Sign out</button>
-      </div>
-  
-
-    </div>
-  
-    </>
-  );
+ return(
+  <Layout>
+    Test
+  </Layout>
+ );
 }
---
+++ app/components/Layout.js
@@ -0,0 +1,35 @@
+"use client";
+
+import { useSession, signIn, signOut } from "next-auth/react";
+import Nav from "./Nav";
+
+export default function Layout({children}) {
+  const { data: session } = useSession();
+  if (!session) {
+    return (
+      <div className="bg-blue-900 w-screen h-screen flex items-center">
+        <div className="text-center w-full">
+          <button onClick={() => signIn('google')} className="bg-white p-2 px-4 rounded-lg">
+            Login With Google
+          </button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <>
+    <div className="bg-red-600 min-h-screen text-white flex">
+      <Nav/>
+      <div className="bg-white flex-grow text-black mt-2 mr-2 mb-0 p-10 rounded-lg p-4">
+
+      <p>{children}</p>
+      <button onClick={() => signOut()}>Sign out</button>
+      </div>
+  
+
+    </div>
+  
+    </>
+  );
+}
