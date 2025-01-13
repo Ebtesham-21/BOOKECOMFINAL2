c7c333b79d79dea2723f2f0329e0d89c943c763b
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-13 15:58:40 +0600

    new product page

--- app/newProducts/page.js
+++ app/newProducts/page.js
@@ -3,8 +3,14 @@ import Layout from "../components/Layout";
 export default function newProducts() {
     return (
         <Layout>
+            <h1 >New Product</h1>
+            <label>Product Name</label>
             <input type="text" placeholder="Book Name"/>
+            <label>Description</label>
             <textarea placeholder="description"></textarea>
+            <label>Price(in USD)</label>
+            <input type="number" placeholder="price"/>
+            <button >Save</button>
         </Layout>
     )
 }
