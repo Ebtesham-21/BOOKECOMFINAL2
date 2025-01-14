5c3c2a7547d6558f3a303db3ccd2475c5a36b0cf
Ebtesham-21 <ebteshamhossain98@gmail.com>
2025-01-14 18:02:33 +0600

    db connected of products sucessfull

--- app/products/page.js
+++ app/products/page.js
@@ -1,8 +1,9 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import { useEffect } from "react";
 
 export default function Products() {
-
+    useEffect(() => {}, []);
     return (
         <Layout>
             <Link className="bg-red-600 rounded-md text-white py-2 px-2" href={'/newProducts'}>
